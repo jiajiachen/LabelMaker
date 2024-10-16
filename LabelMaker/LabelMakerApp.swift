0780b24f5056b830d97d55fe803464267a1bb615
KellyChen <matijueming@gmail.com>
2024-10-16 19:24:02 +0800

    Initial Commit

---
+++ LabelMaker/LabelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelMakerApp.swift
+//  LabelMaker
+//
+//  Created by Jia Chen on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct LabelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
